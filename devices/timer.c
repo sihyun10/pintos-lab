5475bc77bf7bc2c7bfc2cbd6a68f347128a92679
hyeona01 <sha0209@dgu.ac.kr>
2025-05-12 11:44:41 +0900

    feat: PROJECT1/Alarm Clock 구현 #1

--- devices/timer.c
+++ devices/timer.c
@@ -7,6 +7,8 @@
 #include "threads/io.h"
 #include "threads/synch.h"
 #include "threads/thread.h"
+/* 추가 */
+#include "list.h"
 
 /* See [8254] for hardware details of the 8254 timer chip. */
 
@@ -21,166 +23,213 @@
 static int64_t ticks;
 
 /* Number of loops per timer tick.
-   Initialized by timer_calibrate(). */
+	 Initialized by timer_calibrate(). */
 static unsigned loops_per_tick;
 
+/* sleep 중인 쓰레드 목록을 관리 */
+struct list sleep_list;
+
 static intr_handler_func timer_interrupt;
-static bool too_many_loops (unsigned loops);
-static void busy_wait (int64_t loops);
-static void real_time_sleep (int64_t num, int32_t denom);
+static bool too_many_loops(unsigned loops);
+static void busy_wait(int64_t loops);
+static void real_time_sleep(int64_t num, int32_t denom);
 
 /* Sets up the 8254 Programmable Interval Timer (PIT) to
-   interrupt PIT_FREQ times per second, and registers the
-   corresponding interrupt. */
-void
-timer_init (void) {
+	 interrupt PIT_FREQ times per second, and registers the
+	 corresponding interrupt. */
+/* 8254 프로그래밍 가능 간격 타이머(PIT)를 설정하여
+	 초당 PIT_FREQ 횟수를 인터럽트하고 해당 인터럽트를 등록합니다. */
+void timer_init(void)
+{
+	list_init(&sleep_list); // sleep_list 초기화
+
 	/* 8254 input frequency divided by TIMER_FREQ, rounded to
-	   nearest. */
-	uint16_t count = (1193180 + TIMER_FREQ / 2) / TIMER_FREQ;
+		 nearest. */
+	uint16_t count = (1193180 + TIMER_FREQ / 2) / TIMER_FREQ; // 10 m/s
 
-	outb (0x43, 0x34);    /* CW: counter 0, LSB then MSB, mode 2, binary. */
-	outb (0x40, count & 0xff);
-	outb (0x40, count >> 8);
+	outb(0x43, 0x34); /* CW: counter 0, LSB then MSB, mode 2, binary. */
+	outb(0x40, count & 0xff);
+	outb(0x40, count >> 8);
 
-	intr_register_ext (0x20, timer_interrupt, "8254 Timer");
+	intr_register_ext(0x20, timer_interrupt, "8254 Timer");
 }
 
 /* Calibrates loops_per_tick, used to implement brief delays. */
-void
-timer_calibrate (void) {
+/* brief delays(짧은 지연)를 구현하는 데 사용되는 loops_per_tick을 보정합니다. */
+void timer_calibrate(void)
+{
 	unsigned high_bit, test_bit;
 
-	ASSERT (intr_get_level () == INTR_ON);
-	printf ("Calibrating timer...  ");
+	ASSERT(intr_get_level() == INTR_ON);
+	printf("Calibrating timer...  ");
 
 	/* Approximate loops_per_tick as the largest power-of-two
-	   still less than one timer tick. */
+		 still less than one timer tick. */
 	loops_per_tick = 1u << 10;
-	while (!too_many_loops (loops_per_tick << 1)) {
+	while (!too_many_loops(loops_per_tick << 1))
+	{
 		loops_per_tick <<= 1;
-		ASSERT (loops_per_tick != 0);
+		ASSERT(loops_per_tick != 0);
 	}
 
 	/* Refine the next 8 bits of loops_per_tick. */
 	high_bit = loops_per_tick;
 	for (test_bit = high_bit >> 1; test_bit != high_bit >> 10; test_bit >>= 1)
-		if (!too_many_loops (high_bit | test_bit))
+		if (!too_many_loops(high_bit | test_bit))
 			loops_per_tick |= test_bit;
 
-	printf ("%'"PRIu64" loops/s.\n", (uint64_t) loops_per_tick * TIMER_FREQ);
+	printf("%'" PRIu64 " loops/s.\n", (uint64_t)loops_per_tick * TIMER_FREQ);
 }
 
 /* Returns the number of timer ticks since the OS booted. */
+/* OS가 부팅된 이후 타이머 틱 수를 반환합니다. */
 int64_t
-timer_ticks (void) {
-	enum intr_level old_level = intr_disable ();
+timer_ticks(void)
+{
+	enum intr_level old_level = intr_disable();
 	int64_t t = ticks;
-	intr_set_level (old_level);
-	barrier ();
+	intr_set_level(old_level);
+	barrier();
 	return t;
 }
 
 /* Returns the number of timer ticks elapsed since THEN, which
-   should be a value once returned by timer_ticks(). */
+	 should be a value once returned by timer_ticks(). */
+/* THEN 이후 경과된 타이머 틱 수를 반환합니다. 이 값은 timer_ticks()에서 반환된 값이어야 합니다. */
 int64_t
-timer_elapsed (int64_t then) {
-	return timer_ticks () - then;
+timer_elapsed(int64_t then)
+{
+	return timer_ticks() - then;
 }
 
 /* Suspends execution for approximately TICKS timer ticks. */
-void
-timer_sleep (int64_t ticks) {
-	int64_t start = timer_ticks ();
+/* 약 TICKS 타이머 틱 동안 실행을 일시 중단합니다. */
+void timer_sleep(int64_t ticks)
+{
+	int64_t start = timer_ticks();
 
-	ASSERT (intr_get_level () == INTR_ON);
-	while (timer_elapsed (start) < ticks)
-		thread_yield ();
+	ASSERT(intr_get_level() == INTR_ON);
+	enum intr_level old_level = intr_disable(); // 동기화 (interruption 비활성화)
+
+	struct thread *curr = thread_current();
+	// thread에 timer 추가
+	curr->timer = timer_ticks() + ticks; // 깨어날 시간
+	// sleep_list에 추가
+	list_push_back(&sleep_list, &curr->elem);
+
+	// thread blocked state
+	thread_block();
+
+	intr_set_level(old_level);
 }
 
 /* Suspends execution for approximately MS milliseconds. */
-void
-timer_msleep (int64_t ms) {
-	real_time_sleep (ms, 1000);
+void timer_msleep(int64_t ms)
+{
+	real_time_sleep(ms, 1000);
 }
 
 /* Suspends execution for approximately US microseconds. */
-void
-timer_usleep (int64_t us) {
-	real_time_sleep (us, 1000 * 1000);
+void timer_usleep(int64_t us)
+{
+	real_time_sleep(us, 1000 * 1000);
 }
 
 /* Suspends execution for approximately NS nanoseconds. */
-void
-timer_nsleep (int64_t ns) {
-	real_time_sleep (ns, 1000 * 1000 * 1000);
+void timer_nsleep(int64_t ns)
+{
+	real_time_sleep(ns, 1000 * 1000 * 1000);
 }
 
 /* Prints timer statistics. */
-void
-timer_print_stats (void) {
-	printf ("Timer: %"PRId64" ticks\n", timer_ticks ());
+void timer_print_stats(void)
+{
+	printf("Timer: %" PRId64 " ticks\n", timer_ticks());
 }
-
+
 /* Timer interrupt handler. */
 static void
-timer_interrupt (struct intr_frame *args UNUSED) {
+timer_interrupt(struct intr_frame *args UNUSED)
+{
 	ticks++;
-	thread_tick ();
+	thread_tick();
+
+	// sleep_list에 있는 원소 모두 확인
+	struct list_elem *e = list_begin(&sleep_list);
+	while (e != list_end(&sleep_list))
+	{
+		struct thread *t = list_entry(e, struct thread, elem);
+		if (t->timer <= ticks)
+		{
+			e = list_remove(e); // 다음 원소
+			thread_unblock(t);
+		}
+		else
+		{
+			e = list_next(e);
+		}
+	}
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
-   tick, otherwise false. */
+	 tick, otherwise false. */
 static bool
-too_many_loops (unsigned loops) {
+too_many_loops(unsigned loops)
+{
 	/* Wait for a timer tick. */
 	int64_t start = ticks;
 	while (ticks == start)
-		barrier ();
+		barrier();
 
 	/* Run LOOPS loops. */
 	start = ticks;
-	busy_wait (loops);
+	busy_wait(loops);
 
 	/* If the tick count changed, we iterated too long. */
-	barrier ();
+	barrier();
 	return start != ticks;
 }
 
 /* Iterates through a simple loop LOOPS times, for implementing
-   brief delays.
+	 brief delays.
 
-   Marked NO_INLINE because code alignment can significantly
-   affect timings, so that if this function was inlined
-   differently in different places the results would be difficult
-   to predict. */
+	 Marked NO_INLINE because code alignment can significantly
+	 affect timings, so that if this function was inlined
+	 differently in different places the results would be difficult
+	 to predict. */
 static void NO_INLINE
-busy_wait (int64_t loops) {
+busy_wait(int64_t loops)
+{
 	while (loops-- > 0)
-		barrier ();
+		barrier();
 }
 
 /* Sleep for approximately NUM/DENOM seconds. */
 static void
-real_time_sleep (int64_t num, int32_t denom) {
+real_time_sleep(int64_t num, int32_t denom)
+{
 	/* Convert NUM/DENOM seconds into timer ticks, rounding down.
 
-	   (NUM / DENOM) s
-	   ---------------------- = NUM * TIMER_FREQ / DENOM ticks.
-	   1 s / TIMER_FREQ ticks
-	   */
+		 (NUM / DENOM) s
+		 ---------------------- = NUM * TIMER_FREQ / DENOM ticks.
+		 1 s / TIMER_FREQ ticks
+		 */
 	int64_t ticks = num * TIMER_FREQ / denom;
 
-	ASSERT (intr_get_level () == INTR_ON);
-	if (ticks > 0) {
+	ASSERT(intr_get_level() == INTR_ON);
+	if (ticks > 0)
+	{
 		/* We're waiting for at least one full timer tick.  Use
-		   timer_sleep() because it will yield the CPU to other
-		   processes. */
-		timer_sleep (ticks);
-	} else {
+			 timer_sleep() because it will yield the CPU to other
+			 processes. */
+		timer_sleep(ticks);
+	}
+	else
+	{
 		/* Otherwise, use a busy-wait loop for more accurate
-		   sub-tick timing.  We scale the numerator and denominator
-		   down by 1000 to avoid the possibility of overflow. */
-		ASSERT (denom % 1000 == 0);
-		busy_wait (loops_per_tick * num / 1000 * TIMER_FREQ / (denom / 1000));
+			 sub-tick timing.  We scale the numerator and denominator
+			 down by 1000 to avoid the possibility of overflow. */
+		ASSERT(denom % 1000 == 0);
+		busy_wait(loops_per_tick * num / 1000 * TIMER_FREQ / (denom / 1000));
 	}
 }
